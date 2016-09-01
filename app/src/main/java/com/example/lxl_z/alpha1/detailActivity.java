547e3554d75de6bb42c731673d733cda3f1df6f4
ssquirrel <lxl_zhang@hotmail.com>
2016-09-02 00:18:34 +0800

    ui day#0

---
+++ app/src/main/java/com/example/lxl_z/alpha1/detailActivity.java
@@ -0,0 +1,15 @@
+package com.example.lxl_z.alpha1;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail);
+
+
+    }
+}
